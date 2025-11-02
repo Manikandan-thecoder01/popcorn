7549775564f7e515541e5b0b2dcc1d6972fb6f71
Manikandan-thecoder01 <manikandan005p@gmail.com>
2025-11-02 20:52:37 +0530

    Added .env and configured API key

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const KEY = "967d3f0b";
+const KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
